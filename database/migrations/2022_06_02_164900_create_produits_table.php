96a343be669c8e7eb3a8f07cb0e2ba1921daa19a
NiangPro <NiangProgrammeur@gmail.com>
2022-06-02 17:26:57 +0000

    creation table produits

---
+++ database/migrations/2022_06_02_164900_create_produits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->text('description')->nullable();
+            $table->string('type');
+            $table->double('tarif');
+            $table->float('taxe');
+            $table->unsignedBigInteger('entreprise_id');
+            $table->foreign('entreprise_id')->references('id')->on('entreprises')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+};
