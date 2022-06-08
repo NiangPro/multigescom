898aad79ff4f803fbbca206f44568a07706bfe4b
NiangPro <NiangProgrammeur@gmail.com>
2022-06-08 10:06:58 +0000

    commit

---
+++ app/Http/Livewire/Reunions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Reunions extends Component
+{
+    public function render()
+    {
+        return view('livewire.reunions');
+    }
+}
