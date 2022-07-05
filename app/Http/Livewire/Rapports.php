9a4b5cf46d2696bb9dff49572b767853d74f834b
alioune fall <fallmidadi1996@gmail.com>
2022-07-05 12:54:03 +0000

    r

--- app/Http/Livewire/Rapports.php
+++ app/Http/Livewire/Rapports.php
@@ -5,7 +5,9 @@ namespace App\Http\Livewire;
 use App\Models\Astuce;
 use App\Models\Depense;
 use App\Models\Vente;
+use App\Models\VenteItem;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 
@@ -17,6 +19,45 @@ class Rapports extends Component
     public $venteMonth;
     public $depenseMonth;
     public $revenus;
+    public $revenusTotal;
+    public $venteTotal;
+    public $depenseTotal;
+
+    public $form = [
+        'date_debut' => '',
+        'date_fin' => ''
+    ];
+
+    protected $rules = [
+        'form.date_debut' => 'required|date',
+        'form.date_fin' => 'required|date',
+    ];
+
+    protected $messages = [
+        'form.date_debut.required' => 'Le date début est requise',
+        'form.date_fin.required' => 'La date fin est requise',
+    ];
+
+    public function search(){
+        if($this->validate()){
+            if($this->form['date_debut']>$this->form['date_debut']){
+                $this->dispatchBrowserEvent("errorDate");
+            }else{
+                $depense = Depense::where('date', $this->form['date_debut'],'>=')->where('date', $this->form['date_fin'],'<=')
+                ->select([DB::raw("SUM(montant) as amount"), 
+                DB::raw("DATE_FORMAT(date, '%m') as month"),
+                DB::raw("DATE_FORMAT(date, '%Y') as year"),])->groupBy('month')->groupBy('year')->get();
+                dd($depense);
+            }
+        }
+    }
+
+    public function totaux($tab){
+        foreach ($tab as $value) {
+            return $value['amount'];
+        }
+    }
+
     
     public function render()
     {
@@ -27,6 +68,21 @@ class Rapports extends Component
         $this->ventes = $this->astuce->getVentes();
         $this->depenseMonth = $this->astuce->getDepensesMonth();
         $this->venteMonth = $this->astuce->getVentesMonth();
+
+        $depense = Depense::where('entreprise_id', Auth()->user()->entreprise_id)->select([
+            DB::raw("SUM(montant) as amount")])->get();
+        $vente = Vente::where('entreprise_id', Auth()->user()->entreprise_id)->select([
+            DB::raw("SUM(montant) as amount")])->get();
+
+        
+        $this->depenseTotal = $this->totaux($depense);
+        $this->venteTotal = $this->totaux($vente);
+        $this->revenusTotal = $this->venteTotal-$this->depenseTotal;
+        
+
+        foreach ($depense as $value) {
+            $this->depenseTotal = $value['amount'];
+        }
         
         return view('livewire.comptable.rapports')->layout('layouts.app', [
             'title' => "Les Rapports",
--- resources/views/livewire/comptable/rapports.blade.php
+++ resources/views/livewire/comptable/rapports.blade.php
@@ -1,123 +1,124 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="card card-primary">
-            <div class="section-title mt-4 ml-4 mb-0">
-                <strong>Comptabilité du mois</strong>
-            </div>  
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            <i class="fas fa-cart-arrow-down"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                            <h4>Ventes</h4>
+<div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="section-title mt-4 ml-4 mb-0">
+                    <strong>Comptabilité du mois</strong>
+                </div>  
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                            <div class="card-icon bg-primary">
+                                <i class="fas fa-cart-arrow-down"></i>
                             </div>
-                            <div class="card-body">
-                            10
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="fas fa-credit-card"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                            <h4>Dépenses</h4>
-                            </div>
-                            <div class="card-body">
-                            42
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="card card-statistic-1">
-                        <div class="card-icon bg-warning">
-                            <i class="fas fa-wallet"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                            <h4>Revenus</h4>
-                            </div>
-                            <div class="card-body">
-                            1,201
-                            </div>
-                        </div>
-                        </div>
-                    </div>     
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-7">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card card-primary">
-                    <div class="section-title mt-4 ml-4 mb-0">
-                        <strong>Recherche du mois</strong>
-                    </div>  
-                    <div class="card-body">
-                        <div >
-                            <div class="form-group d-flex justify-content-around">
-                                <div class="input-group mb-2">
-                                  <div class="input-group-prepend">
-                                    <div class="input-group-text">Du<span class="text-danger">*</span></div>
-                                  </div>
-                                  <input type="date" class="form-control @error('form.date') is-invalid
-                                    @enderror" wire:model="form.date">
-                                </div> 
-                                <div class="input-group mb-2 ml-3">
-                                    <div class="input-group-prepend">
-                                      <div class="input-group-text">Au</div>
-                                    </div>
-                                    <input type="date" class="form-control @error('form.date') is-invalid
-                                      @enderror" wire:model="form.date">
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                <h4>Ventes</h4>
                                 </div>
-                                <button class="btn btn-success btn-sm ml-3" style="height:41px;" >Valider</button>
+                                <div class="card-body">
+                                    {{$venteTotal}}
+                                </div>
                             </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                            <div class="card-icon bg-danger">
+                                <i class="fas fa-credit-card"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                <h4>Dépenses</h4>
+                                </div>
+                                <div class="card-body">
+                                {{$this->depenseTotal}} CFA
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                            <div class="card-icon bg-warning">
+                                <i class="fas fa-wallet"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                <h4>Revenus</h4>
+                                </div>
+                                <div class="card-body">
+                                {{$revenusTotal}} CFA
+                                </div>
+                            </div>
+                            </div>
+                        </div>     
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-7">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="section-title mt-4 ml-4 mb-0">
+                            <strong>Recherche du mois</strong>
+                        </div>  
+                        <div class="card-body">
+                            <form wire:submit.prevent="search">
+                                <div class="form-group d-flex justify-content-around">
+                                    <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">Du<span class="text-danger">*</span></div>
+                                    </div>
+                                    <input type="date" class="form-control @error('form.date_debut') is-invalid
+                                        @enderror" wire:model="form.date_debut">
+                                    </div> 
+                                    <div class="input-group mb-2 ml-3">
+                                        <div class="input-group-prepend">
+                                        <div class="input-group-text">Au</div>
+                                        </div>
+                                        <input type="date" class="form-control @error('form.date_fin') is-invalid
+                                        @enderror" wire:model="form.date_fin">
+                                    </div>
+                                    <button type="submit" class="btn btn-success btn-sm ml-3" style="height:41px;" >Valider</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="section-title mt-4 ml-4 mb-0">
+                            <strong>Rapports Annuels des Ventes et Dépenses</strong>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChartLine" width="949" height="600"></canvas>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-12">
-                <div class="card card-primary">
-                    <div class="section-title mt-4 ml-4 mb-0">
-                        <strong>Rapports Annuels des Ventes et Dépenses</strong>
-                    </div>
-                    <div class="card-body">
-                        <canvas id="myChartLine" width="949" height="600"></canvas>
-                    </div>
+        </div>
+        <div class="col-md-5">
+            <div class="card card-primary">
+                <div class="section-title mt-4 ml-4 mb-0">
+                    <strong>Graphique du mois</strong>
+                </div>
+                <div class="card-body">
+                    <canvas id="myCharBar" width="400" height="400"></canvas>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-md-5">
-        <div class="card card-primary">
-            <div class="section-title mt-4 ml-4 mb-0">
-                <strong>Graphique du mois</strong>
-            </div>
-            <div class="card-body">
-                <canvas id="myCharBar" width="400" height="400"></canvas>
-            </div>
-        </div>
-    </div>
 </div>
-
 @section('js')
 <script>
     // var depenses = `<?php echo $depenses; ?>`;
     var depenseMonth = JSON.parse`<?php echo $depenseMonth; ?>`;
     var venteMonth = JSON.parse`<?php echo $venteMonth; ?>`;
     var revenus = venteMonth - depenseMonth;
-    alert(revenus);
+
     var ctx = document.getElementById('myChartLine').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
