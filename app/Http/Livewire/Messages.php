a8619e44cf2a7566c05826c98dee4faa6794506c
alioune fall <fallmidadi1996@gmail.com>
2022-07-15 11:21:45 +0000

    chat design

--- app/Http/Livewire/Messages.php
+++ app/Http/Livewire/Messages.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Messages extends Component
 {
+    public $current_user;
+
+    public function changeEvent($user_id){
+        $this->current_user = User::where('id', $user_id)->first();
+        dd($this->current_user);
+    }
+
     public function render()
     {
-        return view('livewire.messages')->layout('layouts.app', [
+        return view('livewire.messages',[
+            'users' => User::where('entreprise_id', Auth::user()->entreprise_id)->where('id', '!=' ,Auth::user()->id)->get(),
+            ])->layout('layouts.app', [
             'title' => "Les Messages",
             "page" => "message",
             "icon" => "fa fa-envelope-open"
--- resources/views/livewire/messages.blade.php
+++ resources/views/livewire/messages.blade.php
@@ -3,17 +3,23 @@
         <div class="col-12 col-sm-6 col-lg-4">
           <div class="card">
             <div class="card-header">
-              <h4>Who's Online?</h4>
+                {{-- <h4>Who's Online?</h4> --}}
+                <select class="form-control">
+                    <option value="">Recherche personne</option>
+                    @foreach ($users as $user)
+                      <option default value="{{ $user->id }}">{{ $user->prenom }} {{ $user->nom }}</option>
+                    @endforeach
+                </select>
             </div>
             <div class="card-body">
               <ul class="list-unstyled list-unstyled-border">
-                <li class="media">
+                {{-- <li class="media">
                   <img alt="image" class="mr-3 rounded-circle" width="50" src="../../storage/images/avatar/avatar-1.png">
                   <div class="media-body">
-                    <div class="mt-0 mb-1 font-weight-bold">Hasan Basri</div>
+                    <div class="mt-0 mb-1 font-weight-bold">{{ $current_user->prenom }} {{ $current_user->nom }}</div>
                     <div class="text-success text-small font-600-bold"><i class="fas fa-circle"></i> Online</div>
                   </div>
-                </li>
+                </li> --}}
                 <li class="media">
                   <img alt="image" class="mr-3 rounded-circle" width="50" src="../../storage/images/avatar/avatar-2.png">
                   <div class="media-body">
@@ -42,9 +48,9 @@
         <div class="col-12 col-sm-6 col-lg-8 mt-0">
           <div class="card chat-box" id="mychatbox">
             <div class="card-header">
-              <h4>Chat with Rizal</h4>
+              <h4>Discussion</h4>
             </div>
-            <div class="card-body chat-content" style="background-image:url('../../storage/images/chat-box.jpg'); 
+            <div class="card-body chat-content" style="background-image:url('../../storage/images/chat-box.png'); 
             background-repeat: ROUND; height: 100%; width:100%; position: relative; ">
                 <div class="chat-item chat-left" >
                     <img src="{{asset('storage/images/'.Auth()->user()->profil)}}">
